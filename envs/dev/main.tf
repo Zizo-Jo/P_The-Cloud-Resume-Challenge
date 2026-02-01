9377466fa2d08cd69b8f7c3b4201ab54b66da31c
Zizo-Jo <zizo.xhao@gmail.com>
2026-02-01 21:01:57 +0100

    update

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -9,13 +9,13 @@ provider "aws" { region = "us-east-1" }
 module "backend" {
   source = "../../modules/backend"
   env    = "dev"
-  # 这里指向 src 里的 python 文件
+  
   lambda_source = "${path.module}/../../src/lambda/func.py"
 }
 
 module "frontend" {
   source = "../../modules/frontend"
   bucket_name = var.my_bucket_name
-  # 这里指向 src 里的 html 文件
+  
   html_source = "${path.module}/../../src/website/index.html"
 }
