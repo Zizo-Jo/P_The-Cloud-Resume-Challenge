fd6d592fa5efc7bbaa057c85190f7cc6867c51e9
Zizo-Jo <zizo.xhao@gmail.com>
2026-02-01 21:07:03 +0100

    fix lambda issue

--- src/lambda/func.py
+++ src/lambda/func.py
@@ -1,34 +1,41 @@
 import json
 import boto3
+import os
+from decimal import Decimal
 
-# Connect to dynamo
+# DynamoDB
 dynamodb = boto3.resource('dynamodb')
-# Find our table
-table = dynamodb.Table('visitors-terraform')
+table_name = os.environ.get('TABLE_NAME')
+table = dynamodb.Table(table_name)
 
 def lambda_handler(event, context):
-    # 1. Atomic Update
-    response = table.update_item(
-        Key={
-            'id': 'count'
-        },
-        UpdateExpression='SET visitor_count = visitor_count + :inc',
-        ExpressionAttributeValues={
-            ':inc': 1
-        },
-        ReturnValues="UPDATED_NEW"
-    )
-    
-    # 2. Get the updated value
-    visitor_count = response['Attributes']['visitor_count']
-    
-    # 3. Build the data that frontend will receive
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Origin': '*',  # Allowing any website to use API
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-        },
-        'body': json.dumps({'count': int(visitor_count)})
-    }
+    try:
+        # Atomic increment of visitor count
+        response = table.update_item(
+            Key={'id': 'count'},
+            UpdateExpression="SET visitor_count = if_not_exists(visitor_count, :start) + :inc",
+            ExpressionAttributeValues={
+                ':inc': 1,
+                ':start': 0
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+        
+        
+        visit_count = int(response['Attributes']['visitor_count'])
+        
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',  
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': json.dumps({'count': visit_count})
+        }
+    except Exception as e:
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
