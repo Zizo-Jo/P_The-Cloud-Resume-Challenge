50a1191c47012916a87d946a29329c658e0649fb
Zizo-Jo <zizo.xhao@gmail.com>
2026-02-03 22:10:52 +0100

    Organize file structure

--- modules/frontend/main.tf
+++ modules/frontend/main.tf
@@ -95,17 +95,4 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 }
 
-# Outputs for DNS Validation and CloudFront URL
-output "acm_certificate_cname" {
-  value = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-}
 
-output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.cdn.domain_name
-}
--- modules/frontend/outputs.tf
+++ modules/frontend/outputs.tf
@@ -1,3 +1,15 @@
+# Outputs for DNS Validation and CloudFront URL
+
 output "cloudfront_url" {
   value = aws_cloudfront_distribution.cdn.domain_name
+}
+
+output "acm_certificate_cname" {
+  value = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 }
