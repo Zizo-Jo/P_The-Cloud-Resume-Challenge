b18ffef0dd9458ce7f1489b199d7194f4b3a852d
Zizo-Jo <zizo.xhao@gmail.com>
2026-02-09 18:44:13 +0100

    OAI -> OAC

--- modules/frontend/main.tf
+++ modules/frontend/main.tf
@@ -3,30 +3,40 @@ resource "aws_s3_bucket" "b" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_website_configuration" "web" {
-  bucket = aws_s3_bucket.b.id
-  index_document { suffix = "index.html" }
-}
-
 resource "aws_s3_bucket_public_access_block" "pub" {
   bucket = aws_s3_bucket.b.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "example-oac"
+  description                       = "OAC for S3"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
 
 resource "aws_s3_bucket_policy" "pol" {
   bucket = aws_s3_bucket.b.id
   depends_on = [aws_s3_bucket_public_access_block.pub]
   policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [{
-      Sid       = "PublicRead",
-      Effect    = "Allow",
-      Principal = "*",
-      Action    = "s3:GetObject",
-      Resource  = "${aws_s3_bucket.b.arn}/*"
+      Sid    = "AllowCloudFrontServicePrincipal"
+      Effect = "Allow"
+      Principal = {
+        Service = "cloudfront.amazonaws.com"
+      }
+      Action   = "s3:GetObject"
+      Resource = "${aws_s3_bucket.b.arn}/*"
+      Condition = {
+        StringEquals = {
+          "AWS:SourceArn" = aws_cloudfront_distribution.cdn.arn 
+        }
+      }
     }]
   })
 }
@@ -59,14 +69,9 @@ resource "aws_cloudfront_distribution" "cdn" {
   aliases = ["zihao-cv.site", "www.zihao-cv.site"]
 
   origin {
-    domain_name = aws_s3_bucket_website_configuration.web.website_endpoint
+    domain_name = aws_s3_bucket.b.bucket_regional_domain_name
     origin_id   = "S3-Origin"
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
-    }
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
   }
 
   default_cache_behavior {
